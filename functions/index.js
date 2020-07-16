bf7e72fdca00b62f4cd5a63d48b9bbb24288e656
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-07-16 14:54:19 +0200

    First firebase functions working experiments

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require('firebase-functions');
+const { debug } = require('firebase-functions/lib/logger');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.scheduleTestAt15H30 = functions.pubsub.schedule('30 15 * * *')
+    .timeZone('Europe/Warsaw') //  timezone can be found at https://time.is
+    .onRun((context) => {
+        console.log('This will be run every day at 15:30 Warsaw time!');
+    });
+
+exports.logModificationTime = functions.database.ref('/clicks')
+    .onWrite((change, context) => {
+        console.log('/meczyki/clicks triggered!');
+        const original = change.after.val();
+        console.log('Original value', original);
+        const rnd = Math.random() * 100;
+        console.log('Random value', rnd);
+        return change.after.ref.parent.child('logs/log').set(rnd);
+    });
--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    counter: 0
+  };
+
+  loadClicks = () => {
+    axios.get('https://meczyki.firebaseio.com/clicks.json')
+      .then(res => {
+        console.log('[Get clicks: ] response:', res.data);
+        this.setState({ counter: res.data.clicks });
+      })
+      .catch(err => {
+        console.log('[Get clicks error]:', err);
+      });
+  };
+
+  componentDidMount() {
+    this.loadClicks();
+
+  }
+
+  clickHandler = () => {
+    console.log('Kliknięto');
+    let counter = this.state.counter + 1;
+    axios.put('https://meczyki.firebaseio.com/clicks.json', { clicks: counter })
+      .then(res => {
+        console.log('[Update clicks: ] response:', res.data.clicks);
+        this.setState({ counter: counter });
+      })
+      .catch(err => {
+        console.log('[Update clicks error]:', err);
+      });
+  };
+
+  render() {
+    let counterText = <p>{this.state.counter}</p>;
+    return (
+      <div className="App">
+        <p>Test</p>
+        <button onClick={this.clickHandler}>Click</button>
+        {counterText}
+      </div>
+    );
+  };
+
 }
+
+
 
 export default App;
