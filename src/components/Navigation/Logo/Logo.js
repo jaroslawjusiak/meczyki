34a8a4550bcda53490dee5a12b72a5b23ce8ec4b
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-07-23 20:33:54 +0200

    Navigation complete without animation effects

--- src/components/Navigation/Logo/Logo.js
+++ src/components/Navigation/Logo/Logo.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import classes from './Logo.module.css';
+import UCL from '../../../assets/images/UCL.svg';
 
 const logo = (props) => {
     return (
-        <div className={classes.Logo}>
-            <h4>Logo</h4>
+        <div className={classes.logo}>
+            <img src={UCL} alt="Champions League" />
+            <div className={classes.title}>
+                <h4>Meczyki</h4>
+            </div>            
         </div>
     );
 };
