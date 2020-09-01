c3f6e4b5a27cd8e326ac738416f682dc75abc24a
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-09-01 19:23:17 +0200

    Google SignIn and SignOut working POC

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,13 +1,42 @@
 import React, {Component} from 'react';
+import firebase from '../../firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import classes from './Auth.module.css';
 
 class Auth extends Component{
     state = {
-    
+        isSignedIn: false
+    };
+
+    uiConfig = {
+        signInFlow: "popup",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            signInSuccess: () => {
+                console.log('signInSuccess');
+                return false;
+            }
+        }
+    };
+
+    logout = () => {
+        firebase.auth().signOut()
+        .then((res) => {
+            console.log('[Auth]: signOut - response', res);
+        })
+        .catch((err) => {
+            console.log('[Auth]: signOut - Error', err);
+        })
+        
     };
 
     componentDidMount(){
-
+        firebase.auth().onAuthStateChanged(res => {
+            console.log('[Auth]: onAuthStateChanged - res', res);
+            this.setState({isSignedIn: !!res});
+        });
     };
 
     componentDidUpdate(){
@@ -17,7 +46,24 @@ class Auth extends Component{
     render(){
         return (
             <div>
-                <p>Logowanie</p>
+                <div>Logowanie</div>
+                {
+                    this.state.isSignedIn
+                    ? (
+                        <div>
+                            <div>Signed in!</div>
+                            <div>
+                                <button onClick={this.logout}>Logout</button>
+                            </div>
+                        </div>
+                        
+                      )
+                    : (<StyledFirebaseAuth
+                        uiConfig={this.uiConfig}
+                        firebaseAuth={firebase.auth()}
+                    />)
+                }
+                
             </div>
         );
     };
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+let config = {
+    apiKey: 'AIzaSyDLn7mFLlSF9f85j3-OacPvV07mJdeXzU8',
+    authDomain: 'meczyki.firebaseapp.com'
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
