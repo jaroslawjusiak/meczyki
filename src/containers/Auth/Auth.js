32d9bf27801b1b2d1383faf01865bc3b2ca2e71d
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-09-01 21:59:24 +0200

    Google SignIn with custom buttons

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,40 +1,68 @@
 import React, {Component} from 'react';
 import firebase from '../../firebase';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+//import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
 import classes from './Auth.module.css';
 
 class Auth extends Component{
     state = {
-        isSignedIn: false
-    };
-
-    uiConfig = {
-        signInFlow: "popup",
-        signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID
-        ],
-        callbacks: {
-            signInSuccess: () => {
-                console.log('signInSuccess');
-                return false;
-            }
-        }
-    };
-
-    logout = () => {
-        firebase.auth().signOut()
-        .then((res) => {
-            console.log('[Auth]: signOut - response', res);
-        })
-        .catch((err) => {
-            console.log('[Auth]: signOut - Error', err);
-        })
+        isSignedIn: false,
         
     };
 
+    googleProvider = new firebase.auth.GoogleAuthProvider();
+
+    // uiConfig = {
+    //     signInFlow: "popup",
+    //     signInOptions: [
+    //         firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    //     ],
+    //     callbacks: {
+    //         signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+    //             console.log('authResult: ', authResult);
+    //             return false;
+    //         }
+    //     }
+    // };
+
+    // logout = () => {
+    //     firebase.auth().signOut()
+    //     .then((res) => {
+    //         console.log('[Auth]: signOut - response', res);
+    //     })
+    //     .catch((err) => {
+    //         console.log('[Auth]: signOut - Error', err);
+    //     })
+        
+    // };
+
+    onSubmit = () => {
+        console.log('signIN');
+        firebase.auth().signInWithPopup(this.googleProvider)
+        .then(user => {
+            console.log('user', user);
+            this.setState({ isSignedIn: !!user });
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
+    };
+
+    onLogout = () => {
+        firebase.auth().signOut()
+        .then((res) => {
+            console.log('signOut');
+        })
+        .catch((err) => {
+            console.log('signOut - Error', err);
+        });
+    };
+
     componentDidMount(){
+
         firebase.auth().onAuthStateChanged(res => {
             console.log('[Auth]: onAuthStateChanged - res', res);
+            console.log('app', firebase.auth().app);
             this.setState({isSignedIn: !!res});
         });
     };
@@ -47,7 +75,7 @@ class Auth extends Component{
         return (
             <div>
                 <div>Logowanie</div>
-                {
+                {/* {
                     this.state.isSignedIn
                     ? (
                         <div>
@@ -62,8 +90,27 @@ class Auth extends Component{
                         uiConfig={this.uiConfig}
                         firebaseAuth={firebase.auth()}
                     />)
+                } */}
+                {
+                    this.state.isSignedIn
+                    ? (
+                        <div>
+                            <div>Signed in!</div>
+                            <div>
+                                <button onClick={this.onLogout}>Logout</button>
+                            </div>
+                        </div>
+                        
+                      )
+                    : (
+                        <div>
+                            <div>Please sign in...</div>
+                            <div>
+                                <button onClick={this.onSubmit}>Sign in</button>
+                            </div>
+                        </div>
+                    )
                 }
-                
             </div>
         );
     };
--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,8 @@ import firebase from "firebase";
 
 let config = {
     apiKey: 'AIzaSyDLn7mFLlSF9f85j3-OacPvV07mJdeXzU8',
-    authDomain: 'meczyki.firebaseapp.com'
+    authDomain: 'meczyki.firebaseapp.com',
+    projectId: 'meczyki'
 };
 
 firebase.initializeApp(config);
